8504c672241cc9f54d0bd72f426c667e1263ae69
Irina Baldwin <2007inb@gmail.com>
2019-01-22 13:25:26 +0000

    Add tests for input variations

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -9,6 +9,19 @@ describe Anagram do
       expect(anagram.find_anagrams('silent', 'listen, apple, window')).
       to eq(['listen'])
     end
+
+    it 'can return anagram words given a list with spaces' do
+      expect(anagram.find_anagrams('silent', 'listen apple window')).
+      to eq(['listen'])
+    end
+
+    it('can return anagram words given a mixed case list') do
+      expect(anagram.find_anagrams('listen', "Silent, APPLE, window")).to eq(['silent'])
+    end
+
+    it('returns empty array when no anagrams are found') do
+      expect(anagram.find_anagrams('listen', "apple, window")).to eq([])
+    end
   end
 
 end

60f864f38376e88c447c71b64025a771bd1dbaa6
Irina Baldwin <2007inb@gmail.com>
2019-01-22 18:59:23 +0000

    Switch to class methods for ease of use

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,20 @@
 class Anagram
-  def find_anagrams(subject, str)
+  def self.find_anagrams(subject, str)
     subject = process_word(subject)
     process_input(str).map { |word| word if anagram?(subject, process_word(word)) }.compact
   end
 
   private
 
-  def process_input(input)
+  def self.process_input(input)
     input.split(/\W+/)
   end
 
-  def process_word(word)
+  def self.process_word(word)
     word.downcase.split(//)
   end
 
-  def anagram?(subject, word)
+  def self.anagram?(subject, word)
     (subject - word).empty?
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,38 +2,37 @@ require 'anagram'
 require 'benchmark'
 
 describe Anagram do
-  let (:anagram) { Anagram.new }
 
   describe '#find_anagrams' do
     it 'can return anagram words given a list with commas' do
-      expect(anagram.find_anagrams('silent', 'listen, apple, window'))
+      expect(Anagram.find_anagrams('silent', 'listen, apple, window'))
         .to eq(['listen'])
     end
 
     it 'can return anagram words given a list with spaces' do
-      expect(anagram.find_anagrams('silent', 'listen apple window'))
+      expect(Anagram.find_anagrams('silent', 'listen apple window'))
         .to eq(['listen'])
     end
 
-    it('can return anagram words given a mixed case list') do
-      expect(anagram.find_anagrams('listen', 'silent, APPLE, window')).to eq(['silent'])
+    it 'can return anagram words given a mixed case list' do
+      expect(Anagram.find_anagrams('listen', 'silent, APPLE, window')).to eq(['silent'])
     end
 
-    it('returns empty array when no anagrams are found') do
-      expect(anagram.find_anagrams('listen', 'apple, window')).to eq([])
+    it 'returns empty array when no anagrams are found' do
+      expect(Anagram.find_anagrams('listen', 'apple, window')).to eq([])
     end
 
-    it('can process large numbers of inputs') do
+    it 'can process large numbers of inputs' do
       skip
       n = 1000000
       input = 'word' * n
       puts "Time to process #{n} records"
-      puts Benchmark.measure { anagram.find_anagrams('listen', input) }
+      puts Benchmark.measure { Anagram.find_anagrams('listen', input) }
       expect(true).to eq(true)
     end
 
-    it 'can return anagrams words in the righ case' do
-      expect(anagram.find_anagrams('listen', 'SILENT, APPLE, window')).to eq(['SILENT'])
+    it 'can return anagram words in the right case' do
+      expect(Anagram.find_anagrams('listen', 'SILENT, APPLE, window')).to eq(['SILENT'])
     end
   end
 end
